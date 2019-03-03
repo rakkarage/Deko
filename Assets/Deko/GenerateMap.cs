62cf185e61c52d42fdc0d79fe860da426a05c1c1
Rakka Rage <rakkarage@gmail.com>
2019-03-03 13:48:15 -0500

    x

--- Assets/Deko/GenerateMap.cs
+++ Assets/Deko/GenerateMap.cs
@@ -1,14 +1,23 @@
+using ca.HenrySoftware.Rage;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 [ExecuteInEditMode]
 public class GenerateMap : MonoBehaviour
 {
 	public GenerateMapType Type;
-	private Tilemap[] Layers;
-	[ExecuteInEditMode]
+	private List<Tilemap> _layers = new List<Tilemap>();
+	private PathMap _pathMap;
 	public void Awake()
 	{
-		Layers = GetComponentsInChildren<Tilemap>();
+		_pathMap = GetComponent<PathMap>();
+		_layers.Add(_pathMap.BackMap);
+		_layers.Add(_pathMap.WaterBackMap);
+		_layers.Add(_pathMap.ItemBackMap);
+		_layers.Add(_pathMap.WaterForeMap);
+		_layers.Add(_pathMap.ForeMap);
+		_layers.Add(_pathMap.ItemForeMap);
+		_layers.Add(_pathMap.LightMap);
 	}
 	[ContextMenu("Generate")]
 	public void Generate()
@@ -18,11 +27,10 @@ public class GenerateMap : MonoBehaviour
 	[ContextMenu("Clear")]
 	public void Clear()
 	{
-		// Layers = GetComponentsInChildren<Tilemap>();
-		foreach (var i in Layers)
+		Debug.Log(_layers.Count);
+		foreach (var i in _layers)
 		{
 			i.ClearAllTiles();
-			i.RefreshAllTiles();
 			i.size = new Vector3Int(Type.Width, Type.Height, 1);
 			i.ResizeBounds();
 			i.RefreshAllTiles();
