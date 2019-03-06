dafbf4343c9d0c776cf27888a9f02b216f33cd8d
Rakka Rage <rakkarage@gmail.com>
2019-03-06 09:21:10 -0500

    x

--- Assets/Deko/Script/Theme.cs
+++ Assets/Deko/Script/Theme.cs
@@ -1,17 +1,16 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 [CreateAssetMenu]
 public class Theme : ScriptableObject
 {
-	public TileBase FloorSimple;
-	public TileBase FloorRoom;
 	public TileBase Floor;
-	public TileBase StairsDown;
-	public TileBase StairsUp;
+	public TileBase FloorRoom;
+	public TileBase StairDown;
+	public TileBase StairUp;
 	public TileBase DoorShut;
 	public TileBase DoorOpen;
+	public TileBase DoorBroke;
 	public TileBase Torch;
-	public TileBase WallSimple;
 	public TileBase Wall;
+	public TileBase WallRoom;
 }
