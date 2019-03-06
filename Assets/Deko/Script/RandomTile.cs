b384f8e6a171ed4b94b779f57ad3fb4726f7e6f6
Rakka Rage <rakkarage@gmail.com>
2019-03-06 11:41:49 -0500

    gone

--- Assets/Deko/Script/RandomTile.cs
+++ Assets/Deko/Script/RandomTile.cs
@@ -1,14 +1,23 @@
-﻿namespace UnityEngine.Tilemaps
+﻿using ca.HenrySoftware.Rage;
+namespace UnityEngine.Tilemaps
 {
 	[CreateAssetMenu]
 	public class RandomTile : TileBase
 	{
-		public RandomTileData Data;
+		public TileOrientation Orientation;
+		public bool FlipX => (Orientation & TileOrientation.FlipX) == TileOrientation.FlipX ? Utility.Random.NextBool() : false;
+		public bool FlipY => (Orientation & TileOrientation.FlipY) == TileOrientation.FlipY ? Utility.Random.NextBool() : false;
+		public bool Rot90 => (Orientation & TileOrientation.Rot90) == TileOrientation.Rot90 ? Utility.Random.NextBool() : false;
+		public static Quaternion RotateClockwise = Quaternion.Euler(0, 0, -90f);
+		public static Quaternion RotateCounter = Quaternion.Euler(0, 0, 90f);
+		public Matrix4x4 NextMatrix => Matrix4x4.TRS(Vector3.zero,
+			Rot90 ? RotateClockwise : Quaternion.identity, new Vector3(FlipX ? -1f : 1f, FlipY ? -1f : 1f, 1f));
+		public WeightedTile Tiles;
 		public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
 		{
-			Data.Tiles.Next.GetTileData(position, tilemap, ref tileData);
+			Tiles.Next.GetTileData(position, tilemap, ref tileData);
 			tileData.flags = TileFlags.LockTransform;
-			tileData.transform = Data.NextMatrix;
+			tileData.transform = NextMatrix;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -6,9 +6,6 @@ art: benmhenry@gmail.com
 
 ## Todo
 
-- combine RandomTile & RandomTileData!!!!!!!!!!!!!!!!!!!!
-- rename theme tiles?
-- is there a way not to dupe floor and wall? and preserve weights? some way to combine wights? Weighted<Weighted>!?
 - make 2d-extras pull requests for randomTile etc
 - save & load? (odin serializer? not working on linux?)
 - add free items etc to free
