36e8c30b8ceb39b6695ad6460d5f5b7827d73d5c
Rakka Rage <rakkarage@gmail.com>
2019-03-06 22:26:08 -0500

    x

--- Assets/Deko/Script/OrientedTile.cs
+++ Assets/Deko/Script/OrientedTile.cs
@@ -4,13 +4,12 @@ namespace UnityEngine.Tilemaps
 	public abstract class OrientedTile : TileBase
 	{
 		public TileOrientation Orientation;
-		public bool FlipX => (Orientation & TileOrientation.FlipX) == TileOrientation.FlipX ? Utility.Random.NextBool() : false;
-		public bool FlipY => (Orientation & TileOrientation.FlipY) == TileOrientation.FlipY ? Utility.Random.NextBool() : false;
-		public bool Rot90 => (Orientation & TileOrientation.Rot90) == TileOrientation.Rot90 ? Utility.Random.NextBool() : false;
-		public static Quaternion RotateClockwise = Quaternion.Euler(0, 0, -90f);
-		public static Quaternion RotateCounter = Quaternion.Euler(0, 0, 90f);
-		public Matrix4x4 NextMatrix => Matrix4x4.TRS(Vector3.zero,
-			Rot90 ? RotateClockwise : Quaternion.identity, new Vector3(FlipX ? -1f : 1f, FlipY ? -1f : 1f, 1f));
+		private bool FlipX => (Orientation & TileOrientation.FlipX) == TileOrientation.FlipX ? Utility.Random.NextBool() : false;
+		private bool FlipY => (Orientation & TileOrientation.FlipY) == TileOrientation.FlipY ? Utility.Random.NextBool() : false;
+		private bool Rot90 => (Orientation & TileOrientation.Rot90) == TileOrientation.Rot90 ? Utility.Random.NextBool() : false;
+		private Matrix4x4 NextMatrix => Matrix4x4.TRS(Vector3.zero,
+			Rot90 ? Quaternion.Euler(0, 0, -90f) : Quaternion.identity,
+			new Vector3(FlipX ? -1f : 1f, FlipY ? -1f : 1f, 1f));
 		public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
 		{
 			tileData.flags = TileFlags.LockTransform;
