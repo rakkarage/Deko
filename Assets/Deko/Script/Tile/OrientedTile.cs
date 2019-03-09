f76f3066f370cd690700f0e5c97b37e16234aa39
Rakka Rage <rakkarage@gmail.com>
2019-03-08 23:06:16 -0500

    OrientedTile

---
+++ Assets/Deko/Script/Tile/OrientedTile.cs
@@ -0,0 +1,20 @@
+using ca.HenrySoftware.Deko;
+namespace UnityEngine.Tilemaps
+{
+	[CreateAssetMenu]
+	public class OrientedTile : TileBase
+	{
+		public TileOrientation Orientation;
+		private bool FlipX => (Orientation & TileOrientation.FlipX) == TileOrientation.FlipX ? Utility.Random.NextBool() : false;
+		private bool FlipY => (Orientation & TileOrientation.FlipY) == TileOrientation.FlipY ? Utility.Random.NextBool() : false;
+		private bool Rot90 => (Orientation & TileOrientation.Rot90) == TileOrientation.Rot90 ? Utility.Random.NextBool() : false;
+		private Matrix4x4 NextMatrix => Matrix4x4.TRS(Vector3.zero,
+			Rot90 ? Quaternion.Euler(0, 0, -90f) : Quaternion.identity,
+			new Vector3(FlipX ? -1f : 1f, FlipY ? -1f : 1f, 1f));
+		public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+		{
+			tileData.flags = TileFlags.LockAll;
+			tileData.transform = NextMatrix;
+		}
+	}
+}
--- Assets/Deko/Script/Tile/RandomTile.cs
+++ Assets/Deko/Script/Tile/RandomTile.cs
@@ -2,21 +2,13 @@
 namespace UnityEngine.Tilemaps
 {
 	[CreateAssetMenu]
-	public class RandomTile : TileBase
+	public class RandomTile : OrientedTile
 	{
-		public TileOrientation Orientation;
-		private bool FlipX => (Orientation & TileOrientation.FlipX) == TileOrientation.FlipX ? Utility.Random.NextBool() : false;
-		private bool FlipY => (Orientation & TileOrientation.FlipY) == TileOrientation.FlipY ? Utility.Random.NextBool() : false;
-		private bool Rot90 => (Orientation & TileOrientation.Rot90) == TileOrientation.Rot90 ? Utility.Random.NextBool() : false;
-		private Matrix4x4 NextMatrix => Matrix4x4.TRS(Vector3.zero,
-			Rot90 ? Quaternion.Euler(0, 0, -90f) : Quaternion.identity,
-			new Vector3(FlipX ? -1f : 1f, FlipY ? -1f : 1f, 1f));
 		public WeightedTile Tiles;
 		public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
 		{
 			Tiles.Next.GetTileData(position, tilemap, ref tileData);
-			tileData.flags = TileFlags.LockAll;
-			tileData.transform = NextMatrix;
+			base.GetTileData(position, tilemap, ref tileData);
 		}
 	}
 }
