aeb447931a92199b4df334d28a305ca28fa34b6b
Rakka Rage <rakkarage@gmail.com>
2019-03-25 18:56:44 -0400

    test

--- Assets/Deko/Script/Data/Item/Slot.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace ca.HenrySoftware.Deko
-{
-	public class Slot : ScriptableObject
-	{
-		public Sprite Icon;
-		public bool Equipable;
-	}
-}
--- Assets/Deko/Script/Data/Stat.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace ca.HenrySoftware.Deko
-{
-	[CreateAssetMenu]
-	public class Stat : ScriptableObject
-	{
-		public Sprite Icon;
-	}
-}
