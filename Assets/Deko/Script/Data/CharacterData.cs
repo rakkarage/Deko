5ef4271362f1ba24f5bf0fb35387e01b5d1d7d92
Rakka Rage <rakkarage@gmail.com>
2019-03-26 21:58:37 -0400

    x

--- Assets/Deko/Script/Data/CharacterData.cs
+++ Assets/Deko/Script/Data/CharacterData.cs
@@ -4,6 +4,6 @@ namespace ca.HenrySoftware.Deko
 	[CreateAssetMenu]
 	public class CharacterData : ScriptableObject
 	{
-		public Vector3Int Position = new Vector3Int(3, 3, 0);
+		public Vector3Int Position = new Vector3Int(2, 2, 0);
 	}
 }
